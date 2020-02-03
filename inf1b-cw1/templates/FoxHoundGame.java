7e1f0284cfccb8055e65a9758fcaddbc22023b33
Advaith Sai <advaith.maddipatla@gmail.com>
2020-02-03 14:36:15 +0000

    game documentation

--- inf1b-cw1/templates/FoxHoundGame.java
+++ inf1b-cw1/templates/FoxHoundGame.java
@@ -33,7 +33,7 @@ public class FoxHoundGame {
      * @param currentTurn last figure to be moved
      * @return next figure to be moved
      */
-    private static char swapPlayers(char currentTurn) {
+    private static char swapPlayers(char currentTurn) { // to swap between hounds and foxes
         if (currentTurn == FoxHoundUtils.FOX_FIELD) {
             return FoxHoundUtils.HOUND_FIELD;
         } else {
@@ -51,69 +51,69 @@ public class FoxHoundGame {
     private static void gameLoop(int dim, String[] players) {
 
         // start each game with the Fox
-        char turn = FoxHoundUtils.FOX_FIELD;
+        char turn = FoxHoundUtils.FOX_FIELD; // we always start with the fox
         boolean exit = false;
         while(!exit) {
-            System.out.println(Arrays.toString(players));
+            System.out.println(Arrays.toString(players)); //printing the players before we choose what move to make. 
             System.out.println("\n#################################");
-            FoxHoundUI.displayBoard(players, dim);
-            int choice = FoxHoundUI.mainMenuQuery(turn, STDIN_SCAN);
-            // handle menu choice
+            FoxHoundUI.displayBoard(players, dim); //displaying the board using the UI function
+            int choice = FoxHoundUI.mainMenuQuery(turn, STDIN_SCAN); // giving the user with main menu options
+            // handle menu choice using a switch condition
             switch(choice) {
                 case FoxHoundUI.MENU_MOVE:
-                    boolean g = false;
+                    boolean g = false; // to break out of the loop if the move is not valid
                     int c = 0;
-                    String[] k = new String[2];
+                    String[] k = new String[2]; // storing the origin and destination
                     do
                     {
                         if (c>0)
-                            System.out.println("invalid input");
+                            System.out.println("invalid input"); // since the code is giving the user multiple chances to enter coordinates, the code will give an error message once there is an issue with the coordinates
                         // System.out.println(FoxHoundUtils.isFoxWin(k[0]));
                         // System.out.println(turn == 'F');
-                        String current_fox = players[players.length-1];
+                        String current_fox = players[players.length-1]; // the last string is the current position of the fox. 
                         if(FoxHoundUtils.isFoxWin(current_fox))
-                        {
+                        { // break out of the code if the fox has already won. The code need not go ahead.
                             System.out.println("Fox has won");
                             System.exit(0);
                         }
                         if(FoxHoundUtils.isHoundWin(players,dim))
-                        {
+                        {// break out of the code if the hound has already won. The code need not go ahead. 
                             System.out.println("Hound has won");
                             System.exit(0);
                         }
-                        k = FoxHoundUI.positionQuery(dim, STDIN_SCAN);
-                        g = FoxHoundUtils.isValidMove(dim, players, turn, k[0], k[1]);
-                        c++;
+                        k = FoxHoundUI.positionQuery(dim, STDIN_SCAN); // asking the position, if no character has won the game yet
+                        g = FoxHoundUtils.isValidMove(dim, players, turn, k[0], k[1]); // storing if the move valid
+                        c++; // since its a do-while the code will work at least once. Hence we need to make sure we prevent that
                     }while(!g);   
-                        int d = find(players,k[0]);
-                        players[d] = k[1];
-                        turn = swapPlayers(turn);
-                        System.out.println(Arrays.toString(players));  
+                        int d = find(players,k[0]); // the find function does a linear search to find the position of the origin and replace with destination
+                        players[d] = k[1]; // replacing with destination
+                        turn = swapPlayers(turn); //swapping
+                        System.out.println(Arrays.toString(players)); // printing after swapping.
         
                     break;
                 case FoxHoundUI.SAVE_MOVE:
-                    Path p = FoxHoundUI.fileQuery(STDIN_SCAN);
-                    boolean b = FoxHoundIO.saveGame(players,turn,p);
-                    if (!(b))
-                        System.err.println("ERROR: Saving file failed.");
+                    Path p = FoxHoundUI.fileQuery(STDIN_SCAN); // taking a scanner object 
+                    boolean b = FoxHoundIO.saveGame(players,turn,p); // saving the players position, if true then quietly saved. else fail the function
+                    if (!(b)) 
+                        System.err.println("ERROR: Saving file failed."); // if theres a problem, print the error
                     break;
                 case FoxHoundUI.LOAD_MOVE:
-                    Path p1 = FoxHoundUI.fileQuery(STDIN_SCAN);
-                    char ch = FoxHoundIO.loadGame(players,p1);
+                    Path p1 = FoxHoundUI.fileQuery(STDIN_SCAN); // taking a scanner object to get the file path
+                    char ch = FoxHoundIO.loadGame(players,p1); // loading the game using the file path and players
                     if (ch == '#')
-                        System.err.println("ERROR: Loading from file failed.");
+                        System.err.println("ERROR: Loading from file failed."); // quietly loads it if given 'H' or 'F', else give an error
                     break;
                 case FoxHoundUI.MENU_EXIT:
                     exit = true;
-                    break;
+                    break; // break and exit the conditional
                 default:
                     System.err.println("ERROR: invalid menu choice: " + choice);
             }
         }
     }
     public static int find(String[] players, String w)
-    {
-        int k = 0;
+    {//linear search to find the position of string object to be replaced. 
+        int k = 0; 
         for(int i = 0;i<players.length;i++)
         {
             if(players[i].equals(w))
@@ -138,23 +138,10 @@ public class FoxHoundGame {
      */
     public static void main(String[] args) throws IOException {
 
-        int dimension = FoxHoundUtils.DEFAULT_DIM;
-        String[] players = FoxHoundUtils.initialisePositions(dimension);
-        // System.out.println(Arrays.toString(players));
-        // System.out.println(FoxHoundUtils.isValidMove(8, players, 'H', "H1", "G2"));
-        // FoxHoundUI.displayBoard(players,dimension);
-        // System.out.println(Arrays.toString(players));
-        // String[] players = {"C2","D1","F1","H1","D7"};
-        Path input = Paths.get("invalidGame06.txt");
-        char fig  = 'F';
-        System.out.println(Arrays.toString(players));
-        System.out.println(FoxHoundIO.loadGame(players, input));
-        System.out.println(Arrays.toString(players));
-        // FoxHoundUI.positionQuery(8, STDIN_SCAN);
-        // Close the scanner reading the standard input stream
-		// Path p1 = Paths.get("/afs/inf.ed.ac.uk/user/s19/s1904223/Inf1B-Coursework/inf1b-cw1/data/invalidGame06.txt");
-        // System.out.println(FoxHoundIO.loadGame(players, p1));
-
-        STDIN_SCAN.close();
+        // main method
+        int dimension = FoxHoundUtils.DEFAULT_DIM; //accessing the default dimension variable from Utils
+        String[] players = FoxHoundUtils.initialisePositions(dimension); //initializing the players
+        gameLoop(dimension,players); //gameLoop function
+        STDIN_SCAN.close();  //closing the scanner object
     }
 }
